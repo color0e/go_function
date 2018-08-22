3f423d42fe8beb29f3e8652a59148a3b0886682c
color0e <limjangsoon@naver.com>
2018-08-22 22:58:08 +0900

    function literal

---
+++ funcliteral/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	func(n int) {
+		fmt.Println(n)
+	}(5)
+
+	printslice := func(strs []string) {
+		for _, str := range strs {
+			fmt.Println(str)
+		}
+	}
+	printslice([]string{"hello", "js", "world"})
+}
